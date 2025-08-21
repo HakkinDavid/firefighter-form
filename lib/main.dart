025c4c325491562fc35b92c95178e51c3d51be71
HakkinDavid <desr11@hotmail.com>
2025-08-20 17:24:15 -0700

    heads up, vamos a iniciar

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:bomberos/routes/home.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      title: 'Aplicación de Atención Prehospitalaria y Servicios Digitales para Bomberos',
+      theme: CupertinoThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        primaryColor: CupertinoColors.systemPurple
+      ),
+      home: const MyHomePage(title: 'Aplicación de Atención Prehospitalaria y Servicios Digitales para Bomberos Home Page'),
+    );
+  }
+}
