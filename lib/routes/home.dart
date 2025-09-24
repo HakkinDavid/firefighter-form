b4e364fbfd7aedf34773a80acc45d16430c72933
HakkinDavid <desr11@hotmail.com>
2025-09-23 18:21:14 -0700

    mejor pantalla de inicio (falta listar formularios, etc.)

--- lib/routes/home.dart
+++ lib/routes/home.dart
@@ -1,4 +1,5 @@
 import 'package:bomberos/models/settings.dart';
+import 'package:bomberos/viewmodels/header.dart';
 import 'package:flutter/cupertino.dart';
 
 class Home extends StatefulWidget {
@@ -33,53 +34,35 @@ class _HomeState extends State<Home> {
     });
   }
 
+  void _createForm() {
+    Navigator.pushNamed(context, '/form');
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: CupertinoColors.activeBlue,
-        // Here we take the value from the Home object that was created by
-        // the App.build method, and use it to set our appbar title.
-        middle: Text("Aplicación de Atención Prehospitalaria y Servicios Digitales para Bomberos"),
-        trailing: CupertinoButton(onPressed: _incrementCounter, child: Icon(CupertinoIcons.add)),
-      ),
-      child: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      navigationBar: null,
+      backgroundColor: Settings().colors.primaryContrast,
+      child: SafeArea(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('Presionaste el botón estas veces'),
-            Text(
-              '$_counter',
-              style: CupertinoTheme.of(context).textTheme.navTitleTextStyle,
+          children: [
+            Header(username: "Blaner", adminUsername: "Villegas"),
+            Expanded(
+              child: Container(
+                color: Settings().colors.background,
+                child: Center(
+                  child: CupertinoButton(
+                    onPressed: _createForm,
+                    color: Settings().colors.primaryContrast,
+                    borderRadius: BorderRadius.circular(48),
+                    child: Icon(CupertinoIcons.add, color: Settings().colors.primary, size: 36,),
+                  ),
+                ),
+              ),
             ),
-            Image.asset('assets/splash.png')
           ],
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
