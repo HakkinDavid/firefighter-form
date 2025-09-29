4d3a29752561ace1b92a32f7e66cf57ddaa7db78
HakkinDavid <desr11@hotmail.com>
2025-09-29 01:50:56 -0700

    conocer a tu jefe

--- database/99.2 - policies.sql
+++ database/99.2 - policies.sql
@@ -1,11 +1,11 @@
 -- 01
 DROP POLICY IF EXISTS user_name_select ON user_name;
 create policy user_name_select on user_name
-for select using (auth.uid() = id or is_under_my_watch(id));
+for select using (auth.uid() = id or is_under_my_watch(id) or is_watching_me(id));
 
 DROP POLICY IF EXISTS user_role_select ON user_role;
 create policy user_role_select on user_role
-for select using (auth.uid() = id or is_under_my_watch(id));
+for select using (auth.uid() = id or is_under_my_watch(id) or is_watching_me(id));
 
 DROP POLICY IF EXISTS user_hierarchy_select ON user_hierarchy;
 create policy user_hierarchy_select on user_hierarchy
