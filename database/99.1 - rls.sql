026b32dcec11d43075dc7d716a10aaac4b4cbccb
HakkinDavid <desr11@hotmail.com>
2025-09-28 19:27:40 -0700

    rls

---
+++ database/99.1 - rls.sql
@@ -0,0 +1,5 @@
+-- Para las políticas, véase 99.2 - policies.sql
+-- 01
+alter table user_name enable row level security;
+alter table user_role enable row level security;
+alter table user_hierarchy enable row level security;
