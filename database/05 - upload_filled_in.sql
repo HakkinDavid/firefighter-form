1e88720ca8f70b33a7642ee6590ea93873dee73c
HakkinDavid <desr11@hotmail.com>
2025-09-28 21:26:11 -0700

    cargar formulario

---
+++ database/05 - upload_filled_in.sql
@@ -0,0 +1,13 @@
+create or replace function upload_filled_in (
+  p_id uuid,
+  p_template_id uuid,
+  p_content jsonb,
+  p_filled_at timestamp
+) returns void as $$
+begin
+  INSERT INTO filled_in (id, template_id, filler, status, content, filled_at) VALUES (p_id, p_template_id, auth.uid(), 2, p_content, p_filled_at);
+exception
+  when others then
+    raise;
+end;
+$$ LANGUAGE plpgsql security definer;
