314c3e603392d58b758b190ba728f528026579ec
HakkinDavid <desr11@hotmail.com>
2025-09-28 21:09:37 -0700

    corregir nombre de columna

--- database/00 - boot.sql
+++ database/00 - boot.sql
@@ -3,7 +3,7 @@ drop table if exists dict_roles;
 -- Diccionario para tipos de usuario (roles)
 CREATE TABLE dict_roles (
     id           smallserial PRIMARY KEY,
-    valor        text        UNIQUE NOT NULL
+    name        text        UNIQUE NOT NULL
 );
 
-INSERT INTO diccionario_tipo_usuario (id, valor) VALUES (0, 'bombero'), (1, 'supervisor'), (2, 'administrador');
+INSERT INTO diccionario_tipo_usuario (id, name) VALUES (0, 'bombero'), (1, 'supervisor'), (2, 'administrador');
