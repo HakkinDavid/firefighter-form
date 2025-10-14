e615b939ff2395a824599de1ac7b52aed44b1f4d
HakkinDavid <desr11@hotmail.com>
2025-10-14 02:34:04 -0700

    permitir la carga de formularios no finalizados

--- database/05 - upload_filled_in.sql
+++ database/05 - upload_filled_in.sql
@@ -1,11 +1,12 @@
 create or replace function upload_filled_in (
   p_id uuid,
   p_template_id uuid,
+  p_status smallint,
   p_content jsonb,
   p_filled_at timestamp
 ) returns void as $$
 begin
-  INSERT INTO filled_in (id, template_id, filler, status, content, filled_at) VALUES (p_id, p_template_id, auth.uid(), 2, p_content, p_filled_at);
+  INSERT INTO filled_in (id, template_id, filler, status, content, filled_at) VALUES (p_id, p_template_id, auth.uid(), p_status, p_content, p_filled_at);
 exception
   when others then
     raise;
