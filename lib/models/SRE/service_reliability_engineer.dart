ddd24c013ba8ff45e53695ca8f2d542d5b8084d0
HakkinDavid <desr11@hotmail.com>
2025-10-24 18:50:16 -0700

    corregir el tipado de funciones

--- lib/models/SRE/service_reliability_engineer.dart
+++ lib/models/SRE/service_reliability_engineer.dart
@@ -5,7 +5,8 @@ import 'Heuristic/disk_heuristic.dart';
 import 'Task/task.dart';
 
 class ServiceReliabilityEngineer {
-  static final ServiceReliabilityEngineer instance = ServiceReliabilityEngineer._internal();
+  static final ServiceReliabilityEngineer instance =
+      ServiceReliabilityEngineer._internal();
 
   ServiceReliabilityEngineer._internal();
 
@@ -20,14 +21,19 @@ class ServiceReliabilityEngineer {
   void initialize() {
     tasks["LoadFromDisk"] = Task(
       heuristic: DiskHeuristic(),
-      trueTask: timeDiskTask(Settings.instance.loadFromDisk)
+      trueTask: timeDiskTask(Settings.instance.loadFromDisk),
     );
     tasks["SetForms"] = Task(
-        heuristic: ConnectionHeuristic(),
-        trueTask: () => Settings.instance.setForms
+      heuristic: ConnectionHeuristic(),
+      trueTask: Settings.instance.setForms,
+    );
+    tasks["SyncForms"] = Task(
+      heuristic: ConnectionHeuristic(),
+      trueTask: Settings.instance.syncForms,
+      dependsOn: {"LoadFromDisk"},
     );
 
-    enqueueTasks({"LoadFromDisk"});
+    enqueueTasks({"LoadFromDisk", "SetForms", "SyncForms"});
   }
 
   void enqueueTasks(Iterable<String> newTasks) {
@@ -47,7 +53,8 @@ class ServiceReliabilityEngineer {
     _isWorking = true;
     tasks.forEach((key, value) async {
       if (_tasksQueue.contains(key)) {
-        await value.runTask();
+        if (value.dependsOn.every((dependency) => !tasks[dependency]!.ready))
+          await value.runTask();
         if (!value.ready) {
           _tasksQueue.removeWhere((t) => t == key);
         }
@@ -75,11 +82,13 @@ class ServiceReliabilityEngineer {
   // }
 
   // Temporary function while all disk logic is being moved from Settings to here
-  void Function() timeDiskTask(void Function() func) {
+  Future<void> Function() timeDiskTask(Future<void> Function() func) {
     return () async {
       DateTime start = DateTime.now();
-      await Future.value(func);
-      DiskHeuristic.lastWriteTime = DateTime.now().difference(start).inMilliseconds;
+      await func();
+      DiskHeuristic.lastWriteTime = DateTime.now()
+          .difference(start)
+          .inMilliseconds;
       DiskHeuristic.lastWriteTimestamp = DateTime.now();
     };
   }
