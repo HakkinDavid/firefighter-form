5968f629b157285e90319b3b45a96327cc753c25
HakkinDavid <desr11@hotmail.com>
2025-10-24 18:02:54 -0700

    eliminar protocolo del internetaddress lookup

--- lib/models/SRE/Heuristic/connection_heuristic.dart
+++ lib/models/SRE/Heuristic/connection_heuristic.dart
@@ -5,7 +5,7 @@ class ConnectionHeuristic extends Heuristic {
   @override
   Future<bool> testHeuristic() async {
     try {
-      final result = await InternetAddress.lookup('https://gpmonaitogjvxrfznhef.supabase.co');
+      final result = await InternetAddress.lookup('gpmonaitogjvxrfznhef.supabase.co');
       return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
     } on SocketException catch (_) {
       return false;
