3748adc452248ff14657204df8cdc816edc5d4d9
HakkinDavid <desr11@hotmail.com>
2025-10-24 20:00:52 -0700

    renombrar propiedades de Task
    trueTask a duty
    fallbackTask a dereliction
    ready a pending

--- lib/models/SRE/Task/task.dart
+++ lib/models/SRE/Task/task.dart
@@ -2,25 +2,30 @@ import 'package:bomberos/models/SRE/Heuristic/heuristic.dart';
 
 class Task {
   final Heuristic heuristic;
-  final Future<void> Function() trueTask;
-  final Future<void> Function()? fallbackTask;
+  final Future<void> Function() duty;
+  final Future<void> Function()? dereliction;
   final Set<String> dependsOn;
-  bool ready = false;
+  bool _pending = false;
+
+  bool get pending => _pending;
+
   Task({
     required this.heuristic,
-    required this.trueTask,
-    this.fallbackTask,
+    required this.duty,
+    this.dereliction,
     this.dependsOn = const {},
   });
 
-  void setReady(bool value) => ready = value;
+  void setAsPending() {
+    _pending = true;
+  }
 
   Future<void> runTask() async {
-    if (await heuristic.value) {
-      await trueTask();
-      ready = false;
-    } else if (fallbackTask != null) {
-      await Future.value(fallbackTask!);
+    if (await heuristic.result) {
+      await duty();
+      _pending = false;
+    } else if (dereliction != null) {
+      await dereliction!();
     }
   }
 }
