e21a6a22aa70f1817c7103ee7e0869677c3b3976
HakkinDavid <desr11@hotmail.com>
2025-11-01 05:01:04 -0700

    delete svelte 2

--- src/lib/stores/appStore.svelte.js
+++
@@ -1,6 +0,0 @@
-let resetFn = $state(() => {});
-
-export const appFunctions = {
-    set onReset(fn) {resetFn = fn},
-    softReset: () => resetFn(),
-}
--- src/lib/stores/dialogStore.svelte.js
+++
@@ -1,8 +0,0 @@
-// Singleton para ModalDialog 
-let dialogContent = $state(null);
-
-export const dialog = {
-  get content() { return dialogContent },
-  open: (options) => dialogContent = options,
-  close: () => dialogContent = null
-};
