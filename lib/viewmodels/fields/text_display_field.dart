8109b8190c3656c09f25de5d7df8c198e8e546a7
HakkinDavid <desr11@hotmail.com>
2026-02-25 18:04:47 -0800

    Create text_display_field.dart

---
+++ lib/viewmodels/fields/text_display_field.dart
@@ -0,0 +1,29 @@
+import 'package:bomberos/viewmodels/fields/input_field.dart';
+import 'package:flutter/cupertino.dart';
+
+class TextDisplayField extends InputField {
+  const TextDisplayField({
+    super.key,
+    required super.field,
+    required super.value,
+    required super.formSet,
+    required super.canEditForm,
+    required super.formatOptions,
+  });
+
+  @override
+  State<InputField> createState() => _NumberInputFieldState();
+}
+
+class _NumberInputFieldState extends InputFieldState {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 8),
+      child: Text(
+        widget.field['text'] ?? '',
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
