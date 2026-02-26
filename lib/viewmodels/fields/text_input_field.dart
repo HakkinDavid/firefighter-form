28078155e68fcb34ca4ca02d686753ba628a29b5
HakkinDavid <desr11@hotmail.com>
2026-02-25 18:04:49 -0800

    Create text_input_field.dart

---
+++ lib/viewmodels/fields/text_input_field.dart
@@ -0,0 +1,38 @@
+import 'package:bomberos/viewmodels/fields/input_field.dart';
+import 'package:flutter/cupertino.dart';
+
+class TextInputField extends InputField {
+  const TextInputField({
+    super.key,
+    required super.field,
+    required super.value,
+    required super.formSet,
+    required super.canEditForm,
+    required super.formatOptions,
+  });
+
+  @override
+  State<InputField> createState() => _NumberInputFieldState();
+}
+
+class _NumberInputFieldState extends InputFieldState {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(label, style: TextStyle(fontWeight: FontWeight.w500)),
+        CupertinoTextField(
+          placeholder: label,
+          controller: TextEditingController(text: widget.value)
+            ..selection = TextSelection.collapsed(offset: widget.value.length),
+          onChanged: (val) {
+            setState(() {
+              widget.formSet(widget.field['name'], val);
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
