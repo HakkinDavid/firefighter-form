7b2e421ebb47e65dc4a70a9a7ee56caf2bc89aca
HakkinDavid <desr11@hotmail.com>
2026-03-10 16:44:10 -0700

    Create 08 - set_user_hierarchy.sql

---
+++ database/08 - set_user_hierarchy.sql
@@ -0,0 +1,13 @@
+create or replace function set_user_hierarchy (
+  p_user_id uuid,
+  p_watcher_id uuid
+) returns void as $$
+begin
+  PERFORM only_admins();
+
+  insert into user_hierarchy (id, watched_by) values (p_user_id, p_watcher_id) on conflict (id) do update set watched_by = p_watcher_id;
+exception
+  when others then
+    raise;
+end;
+$$ LANGUAGE plpgsql security definer;
