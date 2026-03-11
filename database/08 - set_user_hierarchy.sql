4cfd07dc9d7d024a66baac3820cfbced00d6b734
HakkinDavid <desr11@hotmail.com>
2026-03-10 18:46:50 -0700

    Update 08 - set_user_hierarchy.sql

--- database/08 - set_user_hierarchy.sql
+++ database/08 - set_user_hierarchy.sql
@@ -1,11 +1,11 @@
 create or replace function set_user_hierarchy (
-  p_user_id uuid,
+  p_watched_id uuid,
   p_watcher_id uuid
 ) returns void as $$
 begin
   PERFORM only_admins();
 
-  insert into user_hierarchy (id, watched_by) values (p_user_id, p_watcher_id) on conflict (id) do update set watched_by = p_watcher_id;
+  insert into user_hierarchy (id, watched_by) values (p_watched_id, p_watcher_id) on conflict (id) do update set watched_by = p_watcher_id;
 exception
   when others then
     raise;
